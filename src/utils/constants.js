f43a99cd48eccab53a0c0119705cf245b8f0500f
Doomfang3 <saubinmathieu@gmail.com>
2022-06-28 11:20:56 +0200

    Add node env check on api url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,4 @@
-export const BASE_API_URL = 'https://m3ftgjnhdgchj.herokuapp.com'
+export const BASE_API_URL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://m3ftgjnhdgchj.herokuapp.com'
+    : 'http://localhost:5005'
